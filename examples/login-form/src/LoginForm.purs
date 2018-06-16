93818aa715289070a6c1d3665f39ef83dee1e331
ashutosh.das <ashutosh.das@juspay.in>
2018-06-16 19:57:49 +0530

    experimenting with adding behaviors as event.

--- examples/login-form/src/LoginForm.purs
+++ examples/login-form/src/LoginForm.purs
@@ -16,19 +16,21 @@ import FRP.Event (create, subscribe)
 import FormField as FormField
 import Halogen.VDom (buildVDom, extract)
 import PrestoDOM.Core (mapDom, getRootNode, insertDom, patchAndRun, spec, storeMachine)
-import PrestoDOM.Events (onClick)
+import PrestoDOM.Events (onClick, attachTimer)
 import PrestoDOM.Types.Core (PrestoDOM, Screen, Eval, Namespace(..), PropEff)
 import PrestoDOM.Utils (continue, continueWithCmd, updateAndExit, exit)
 
 data Action =
   Username FormField.Action
   | Password FormField.Action
+  | TimerAction
   | SubmitClicked
   | SubmitClicked2
 
 type State =
   { errorMessage :: String
   , toggle :: Boolean
+  , timer :: Int
   , usernameState :: FormField.State
   , passwordState :: FormField.State
   }
@@ -37,12 +39,17 @@ initialState :: State
 initialState =
   { errorMessage : ""
   , toggle : true
+  , timer : 30
   , usernameState : (FormField.initialState "username")
   , passwordState : (FormField.initialState "password")
   }
 
 eval :: forall eff. Action -> State -> Eval eff Action Unit State
 eval (Username action) state = continue state { usernameState = FormField.eval action state.usernameState }
+eval TimerAction state = continue $
+    case (state.timer < 0) of
+         false -> state {timer = state.timer - 1}
+         true -> state {timer = 0}
 eval (Password action) state = let t = if state.passwordState.value == "turn" then false else true in continue state { passwordState = FormField.eval action state.passwordState, toggle = t }
 eval SubmitClicked2 state = continue state { errorMessage = "Yes, yo hoo", toggle = true }
 eval SubmitClicked state = continue state { errorMessage = "Your account is blocked", toggle = false }
@@ -66,6 +73,7 @@ view push state =
     [ height MATCH_PARENT
     , width MATCH_PARENT
     , background "#323232"
+    , attachTimer push (const TimerAction)
     , gravity CENTER
     ]
     [ linearLayout
@@ -97,8 +105,8 @@ view push state =
           , color "#000000"
           , background "#ffffff"
           , width MATCH_PARENT
-          , onClick push (const SubmitClicked)
-          , text $ state.passwordState.value <> state.errorMessage
+          {-- , onClick push (const SubmitClicked) --}
+          , text $ show state.timer <> " sec" <> state.passwordState.value <> state.errorMessage
           ]
         , linearLayout
             case state.toggle of
@@ -107,7 +115,7 @@ view push state =
                             , margin $ Margin 20 20 20 20
                             , background "#969696"
                             , gravity CENTER
-                            {-- , onClick push (const SubmitClicked) --}
+                            , onClick push (const SubmitClicked)
                             ])
                  false ->  ([ height $ V 50
                             , width MATCH_PARENT
--- examples/login-form/src/Main.purs
+++ examples/login-form/src/Main.purs
@@ -8,21 +8,19 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Ref (REF)
 import Control.Monad.Eff.Console (CONSOLE, log) as C
 import Control.Monad.Eff.Exception (EXCEPTION)
-import Control.Monad.Eff.Timer as T
 import DOM (DOM)
 import FRP (FRP)
 import LoginForm as LoginForm
 import LoginTest as LoginTest
 import SplashScreen as SplashScreen
-import PrestoDOM.Core (runScreen, initUIWithScreen)
+import PrestoDOM.Core (runScreen, initUIWithScreen, initUI)
 import PrestoDOM.Types.Core (Screen)
 
 main :: forall eff. Eff ( frp :: FRP, dom :: DOM, console :: C.CONSOLE, ref :: REF | eff ) Unit
 main = do
   _ <- launchAff_ do
      log "sojkhkk"
-     _ <- makeAff (\cb -> initUIWithScreen SplashScreen.screen cb)
-     _ <- delay $ Milliseconds 1000.0
+     _ <- makeAff (\cb -> initUI cb)
      log "yo oo oo"
      _ <- runUI LoginForm.screen "2"
      log "fooo"
