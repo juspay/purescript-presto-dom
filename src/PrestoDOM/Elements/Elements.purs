10a55d24787220be340480c6bf47f8fde3815c17
V Shiva Nandan <shiva.nandan@juspay.in>
2020-01-13 15:09:47 +0530

    Add module detection support in development mode

--- src/PrestoDOM/Elements/Elements.purs
+++ src/PrestoDOM/Elements/Elements.purs
@@ -36,6 +36,8 @@ import Data.Tuple (Tuple)
 import Halogen.VDom.DOM.Prop (Prop)
 import PrestoDOM.Types.Core (ElemName(..), VDom(..), Namespace)
 
+foreign import getLoggerComponent :: forall i p. Node (Prop i) p -> Node (Prop i) p
+
 type Node i p
    = Array i
   -> Array (VDom (Array i) p)
@@ -61,10 +63,10 @@ keyed :: forall i p. ElemName -> Array (Prop i) -> Array (Tuple String (VDom (Ar
 keyed elemName = Keyed Nothing elemName
 
 node :: forall i p. String -> Node (Prop i) p
-node elem = element (ElemName elem)
+node elem = getLoggerComponent (element (ElemName elem))
 
 leaf :: forall i p. String -> Leaf (Prop i) p
-leaf elem props = element (ElemName elem) props []
+leaf elem props = node elem props []
 
 
 
