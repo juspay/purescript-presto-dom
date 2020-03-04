ad02eac4d3de673e547d7e8291b70671caae0d07
Georgie <george.james@juspay.in>
2020-03-04 12:17:49 +0530

    added presto dependancy

---
+++ src/PrestoDOM/Core/Types/Language/Flow/Flow.purs
@@ -0,0 +1,20 @@
+module PrestoDOM.Core.Types.Language.Flow where
+
+import Prelude
+
+import Effect.Aff (makeAff)
+import Presto.Core.Flow (Flow, doAff)
+import PrestoDOM (Screen)
+import PrestoDOM.Core (initUI, initUIWithScreen, runScreen, showScreen) as PrestoDOM
+
+initUI :: Flow Unit
+initUI = doAff do makeAff \cb -> PrestoDOM.initUI cb
+
+initUIWithScreen :: forall action state. Screen action state Unit -> Flow Unit
+initUIWithScreen screen = doAff do makeAff \cb -> PrestoDOM.initUIWithScreen screen cb
+
+runScreen :: forall action state retType. Screen action state retType -> Flow retType
+runScreen screen = doAff do makeAff \cb -> PrestoDOM.runScreen screen cb
+
+showScreen :: forall action state retType. Screen action state retType -> Flow retType
+showScreen screen = doAff do makeAff \cb -> PrestoDOM.showScreen screen cb
