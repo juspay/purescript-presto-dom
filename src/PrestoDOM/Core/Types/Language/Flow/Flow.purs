5a0f5f0fe28d595193e31b7c2b2867a7e3f775ca
George James <george.james@juspay.in>
2021-03-22 12:23:20 +0530

    PICAF-3923 :: hijack existing interfaces to use new one

--- src/PrestoDOM/Core/Types/Language/Flow/Flow.purs
+++ src/PrestoDOM/Core/Types/Language/Flow/Flow.purs
@@ -3,17 +3,20 @@ module PrestoDOM.Core.Types.Language.Flow where
 
 import Prelude
 
-import Effect.Aff (makeAff)
+import Data.Maybe (Maybe(..))
+import Effect.Aff (makeAff, nonCanceler)
 import Effect.Class (liftEffect)
 import Presto.Core.Flow (Flow, doAff)
 import Presto.Core.Types.Language.Flow(getLogFields)
 import PrestoDOM (Screen)
-import PrestoDOM.Core (initUI, initUIWithScreen, runScreen, showScreen, updateScreen, prepareScreen) as PrestoDOM
-import PrestoDOM.Types.Core(class Loggable)
+import PrestoDOM.Core (initUIWithScreen, runScreen, showScreen, updateScreen, prepareScreen) as PrestoDOM
+import PrestoDOM.Core2 as PrestoDOM2
+import PrestoDOM.Types.Core (class Loggable, ScopedScreen)
 
 initUI :: Flow Unit
-initUI  = doAff do makeAff \cb -> PrestoDOM.initUI cb
+initUI  = doAff do makeAff \cb -> PrestoDOM2.initUIWithNameSpace "default" Nothing *> pure nonCanceler
 
+-- deprecated
 initUIWithScreen
   :: forall action state
    . Screen action state Unit
@@ -24,7 +27,7 @@ initUIWithScreen screen =
 runScreen :: forall action state retType. Show action => Loggable action => Screen action state retType -> Flow retType
 runScreen screen = do
   json <- getLogFields
-  doAff do makeAff \cb -> PrestoDOM.runScreen screen cb json
+  doAff do makeAff \cb -> PrestoDOM2.runScreen (mapToScopedScreen screen) cb json
 
 prepareScreen
   :: forall action state retType
@@ -37,9 +40,19 @@ prepareScreen screen = do
 showScreen :: forall action state retType. Show action => Loggable action => Screen action state retType -> Flow retType
 showScreen screen = do
   json <- getLogFields
-  doAff do makeAff \cb -> PrestoDOM.showScreen screen cb json
+  doAff do makeAff \cb -> PrestoDOM2.showScreen (mapToScopedScreen screen) cb json
 
 updateScreen :: forall action state retType. Show action => Loggable action => Screen action state retType -> Flow Unit
 updateScreen screen = do
   json <- getLogFields
   doAff do liftEffect $ PrestoDOM.updateScreen screen json
+
+mapToScopedScreen :: forall action state retType. Show action => Loggable action => Screen action state retType -> ScopedScreen action state retType
+mapToScopedScreen screen =
+  { initialState : screen.initialState
+  , name : screen.name
+  , globalEvents : screen.globalEvents
+  , view : screen.view
+  , eval : screen.eval
+  , parent : Nothing
+  }
